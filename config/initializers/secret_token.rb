dce9739c4aeb88eb8a787607163120afce3ce4a7
Peter Kordel <pkordel@gmail.com>
2011-11-05 14:48:33 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+H5bp::Application.config.secret_token = '710b17496d0629130988630b24e7d6b4d820208b353456901e1133f3de67afffeb441b46523831da1bc219921db041438249d59b8b47611262d4a257240a4bb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+H5bp::Application.config.session_store :cookie_store, key: '_h5bp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# H5bp::Application.config.session_store :active_record_store
